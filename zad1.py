546dbe1febfdfc2a400f6a15fd481325928ad39b
kamilskalka <kamilskalka26@gmail.com>
2024-12-05 20:40:48 +0100

    Add files via upload

--- zad1.py
+++ zad1.py
@@ -1,21 +1,39 @@
 #Napisac program realizujacy mnozenie macierzy (gdzie macierze sa reprezentowane przez listy)
 
-matrix_a = [[1, 2, 5], [3, 4, 2]]
-matrix_b = [[5, 6], [7, 8]]
+
+#matrix[row][column]
+def dot_product(a,b):
+    val = 0
+    for i in range(len(a)):
+        val = val + a[i]*b[i]
+    return val
+
 
 def matrix_multi(a,b):
+
     a_height = len(a)
-    a_length = len(a[0])
-
-
     b_height = len(b)
     b_length = len(b[0])
+    b_col = [0] * b_height
+    matrix = [[0] * b_length for _ in range(a_height)]
+
+    for i in range(a_height): #row
+        for j in range(b_length): #column
+            b_col = [b[k][j] for k in range(b_height)]
+            matrix[i][j] = dot_product(a[i],b_col)
 
 
-for i in range(b_height):
-    for j in range(a_length):
-        result[i][j] = (matrix_a[i][0] * matrix_b[0][j] +
-                        matrix_a[i][1] * matrix_b[1][j])
+    return matrix
 
-for row in result:
-    print(row)
+matrix_a = [[1, 2, 3,-7],
+            [3, 2, 1,7]]
+matrix_b = [[5, 6,-1],
+            [4, -6,-1],
+            [1,1,-1],
+            [-2,4,-1]]
+
+matrix_c = matrix_multi(matrix_a,matrix_b)
+print("matrix:")
+for sublist in matrix_c:
+    print(sublist)
+
